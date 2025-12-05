f102bc34c52525515ba22515cbb997bc08432aa3
kushalpra123 <kushalpra49@gmail.com>
2025-12-05 11:44:05 -0500

    added api url and fetch req to CoinGecko

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 //function to load cryptocurrencies
 async function loadCryptos() {
     console.log("Crypto function ready");
+    
+    //  API URL for fetching cryptocurrency data
+    const url =
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&ids=bitcoin,ethereum,solana,dogecoin,ripple,cardano,polkadot,litecoin&order=market_cap_desc";
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data); 
 }
+ loadCryptos();
